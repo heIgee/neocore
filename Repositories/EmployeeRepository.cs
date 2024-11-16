2a1e6de94391e7c0b6d4936536713503529acc8a
heIgee <oleo7979@gmail.com>
2024-11-16 18:18:47 +0200

    Employees & EmployeeForm + simplified binding

--- Repositories/EmployeeRepository.cs
+++ Repositories/EmployeeRepository.cs
@@ -34,4 +34,74 @@ public class EmployeeRepository(IDriver driver) : NeocoreRepository(driver)
             Employee.FromRecord
         );
     }
+
+    public async Task Update(int id, Employee employee)
+    {
+        _ = await FindById(id) ?? throw new InvalidOperationException(@$"
+            Cannot update non-existent {nameof(Employee)} (id: {id})
+        ");
+
+        const string query = @$"
+            MATCH ({Al.Employee}:Employee {{id: $id}})
+            SET {Al.Employee}.fullName = $fullName
+        ";
+
+        var parameters = GetParams(id, employee);
+
+        await ExecuteWriteSingleAsync(
+            query,
+            parameters
+        );
+    }
+
+    public async Task Add(Employee employee)
+    {
+        int id = await NewId();
+
+        var query = @$"
+            CREATE ({Al.Employee}:Employee {{id: $id, fullName: $fullName }})
+        ";
+
+        var parameters = GetParams(id, employee);
+
+        await ExecuteWriteSingleAsync(
+            query.ToString(),
+            parameters
+        );
+    }
+
+    public async Task Delete(int id)
+    {
+        const string query = @$"
+            MATCH ({Al.Employee}:Employee {{id: $id}})
+            DETACH DELETE {Al.Employee}
+        ";
+        
+        await ExecuteWriteSingleAsync(
+            query,
+            new { id }
+        );
+    }
+
+    private async Task<int> NewId()
+    {
+        const string query = @$"
+            MATCH ({Al.Employee}:Employee)
+            RETURN {Al.Employee}.id as id
+            ORDER BY {Al.Employee}.id desc
+            LIMIT 1
+        ";
+
+        return await ExecuteReadSingleAsync(
+            query,
+            new { },
+            r => r["id"].As<int>()
+        ) + 1;
+    }
+
+    private static Dictionary<string, object?> GetParams(int id, Employee employee) => new()
+    {
+        ["id"] = id,
+        ["fullName"] = employee.FullName ?? string.Empty,
+    };
 }
